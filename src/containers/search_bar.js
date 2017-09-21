91a7aa3009858dcaee23d49064490600d1abf68c
kent10ou <kent10ou@gmail.com>
2017-09-21 03:46:15 -0700

    hook up fetchWeather action to form submit, working on reducer

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 class SearchBar extends Component {
   constructor(props) {
@@ -8,6 +11,7 @@ class SearchBar extends Component {
 
     // bind the context of this to SearchBar
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -18,6 +22,8 @@ class SearchBar extends Component {
     event.preventDefault();
     
     // go fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' }); // clear form
   }
 
   render() {
@@ -37,4 +43,8 @@ class SearchBar extends Component {
   }
 }
 
-export default SearchBar;
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
