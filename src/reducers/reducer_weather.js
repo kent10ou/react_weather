c331a8ef18c3158da3c3d6310c0e59f03b9be11a
kent10ou <kent10ou@gmail.com>
2017-09-21 04:15:57 -0700

    create weather_list container, hook up redux state to it

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,7 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 export default function (state = [], action) {
-  switch (aciton.type) {
+  switch (action.type) {
   case FETCH_WEATHER:
     // we want to return a new instance of state, and not mutate our state
     return [ action.payload.data, ...state ]; // instead of 'return state.concat([action.payload.data]);'
